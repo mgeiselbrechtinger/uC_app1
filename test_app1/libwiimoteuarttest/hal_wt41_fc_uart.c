9d1e5f35e3e4517dc193b12eb9f70c21fe52d855
mgeiselbrechtinger <m.geiselbrechtinger@gmail.com>
2018-11-07 21:40:24 +0100

    uart test working

--- test_app1/libwiimoteuarttest/hal_wt41_fc_uart.c
+++ test_app1/libwiimoteuarttest/hal_wt41_fc_uart.c
@@ -6,8 +6,8 @@
 #include    "./hal_wt41_fc_uart.h"
 #include    "./util.h"
 
-#define CTS     (PJ2)
-#define RTS     (PJ3)
+#define CTS     (PJ3)
+#define RTS     (PJ2)
 #define RST     (PJ5)
 #define FOSC    (16000000)
 #define BAUD    (1000000)
@@ -15,9 +15,9 @@
 
 static uint8_t buff_active_flag;
 static uint8_t rcvBuff[32];
+static uint8_t rcv_buff_len;
 static uint8_t head;
 static uint8_t tail;
-static uint8_t rnd_flag;
 
 static uint8_t sndBuff;
 static uint8_t snd_flag;
@@ -27,14 +27,23 @@ static void (*_rcvCallback)(uint8_t rcvData);
 
 error_t halWT41FcUartInit(void (*sndCallback)(), void (*rcvCallback)(uint8_t))
 {
-    // port initialization
-    PORTJ = 0;
-    DDRJ  = (1 << PJ0) | (1 << CTS) | (1 << RST);
-
+    // set RTS as input
+    PORTJ &= ~((1 << RTS));
+	DDRJ  &= ~((1 << RTS));
+	// set CTS as output
+    DDRJ  |= (1 << CTS);
+	PORTJ &= ~(1 << CTS);
+	// set RST as output
+	DDRJ  |= (1 << RST);
+	PORTJ |= (1 << RST);
+	
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
-        // set baudrate to 1M
-        UBRR3H = 0x0F & (int8_t)(UBRR_VAL >> 8);
-        UBRR3L = (int8_t)UBRR_VAL;
+        // set baudrate to 500k
+        UBRR3 = 1;
+		// double baud to 1M
+		UCSR3A |= (1 << U2X3);
+        // UBRR3H = 0x0F & (int8_t)(UBRR_VAL >> 8);
+        // UBRR3L = (int8_t)UBRR_VAL;
         // enable receiver, transmitter and interrupts
         UCSR3B = (1 << RXEN3) | (1 << TXEN3) | (1 << RXCIE3);
         // set frame format, 8data, 1stop bit 
@@ -48,22 +57,23 @@ error_t halWT41FcUartInit(void (*sndCallback)(), void (*rcvCallback)(uint8_t))
     TCCR1A = 0;
     TCCR1B = (1 << WGM12) | (1 << CS10) | (1 << CS12);
     
-    // setup pinchange interrupt for RTS pin
-    PCMSK1 |= (1 << PCINT12);
-    
     // reset BT module
     PORTJ &= ~(1 << RST);
     
-    // setup receive buffer
+	// setup pinchange interrupt for RTS pin
+    PCMSK1 |= (1 << PCINT11);
+    
+    // setup buffers
+    rcv_buff_len = 0;
     head = 0;
     tail = 0;
-    rnd_flag = 0;
     snd_flag = 0;
     buff_active_flag = 0;
     
     // add callback functions
     _sndCallback = sndCallback;
     _rcvCallback = rcvCallback;
+
     return SUCCESS;
 }
 
@@ -71,8 +81,8 @@ error_t halWT41FcUartSend(uint8_t byte)
 {
     sndBuff = byte;
     snd_flag = 1;
-
-    // test if send not possible
+    
+	// test if send possible
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE){
         // reset still active
         if(!(PORTJ  & (1 << RST)))
@@ -86,7 +96,7 @@ error_t halWT41FcUartSend(uint8_t byte)
         // UART tx reg full
         if(!(UCSR3A & (1 << UDRE3))){
             // enable intr on reg empty
-            UCSR3B |=(1 << UDRIE3);
+            UCSR3B |= (1 << UDRIE3);
             return SUCCESS;
         }
     }
@@ -96,6 +106,9 @@ error_t halWT41FcUartSend(uint8_t byte)
     UDR3 = byte;
     // enable empty interrupt
     UCSR3B |= (1 << UDRIE3);
+	
+	// TODO debug
+	PORTB |= 1;
 
     return SUCCESS;
 }
@@ -110,6 +123,9 @@ ISR(TIMER1_COMPA_vect)
     // re-enable BT module
     PORTJ |= (1 << RST);
     
+	// TODO debug
+	PORTB |= 2;
+
     sei();
     // check for buffered msg
     if(snd_flag == 1)
@@ -119,29 +135,18 @@ ISR(TIMER1_COMPA_vect)
 // UART receive interrupt
 ISR(USART3_RX_vect)
 {
-    uint8_t free;
-
     rcvBuff[head] = UDR3;
-    head += 1;
+	head = (head + 1) & 31;
+	rcv_buff_len++;
+	
+	if((32 - rcv_buff_len) < 5)
+		PORTJ |= (1 << CTS);
 
-    // buffer ovf flag
-    if(head > 31){
-        head = 0;
-        rnd_flag = 1;
-    }
-    // calculate free buffer space
-    if(rnd_flag == 0)
-        free = 32 - head - tail;
-    else
-        free = tail - head;
-
-    // buffer too full, set CTS
-    if(free < 5)
-        PORTJ |= (1 << CTS);
+	// TODO debug
+	PORTB |= 4;
 
     // empty ringbuffer
-    sei();
-    if(buff_active_flag = 0)
+    if(buff_active_flag == 0)
         empty_buffer();
 
 }
@@ -149,32 +154,28 @@ ISR(USART3_RX_vect)
 static void empty_buffer()
 {
     buff_active_flag = 1;
-    
-    // buffer ovf occured
-    if(rnd_flag == 1){
-        for(; tail < 32; tail++){
-            _rcvCallback(rcvBuff[tail]);
-            // buffer empty enough, release CTS
-            if((32 - tail - head) < 16)
-                PORTJ &= ~(1 << CTS);
-        }
-        tail = 0;
-        rnd_flag = 0;
-    }
-    // no buffer ovf
-    for(; tail < head; tail++){
-        _rcvCallback(rcvBuff[tail]);
-        // buffer empty enough, release CTS
-        if((head - tail) < 16)
-            PORTJ &= ~(1 << CTS);
-    }
+	// re-enable interrupts
+	sei();
+   	// TODO debug
+   	PORTB |= 32;
 
+	for(; rcv_buff_len > 0; rcv_buff_len--){
+		_rcvCallback(rcvBuff[tail]);
+		tail = (tail + 1) & 31;
+
+		if(rcv_buff_len < 16)
+			PORTJ &= ~(1 << CTS);
+	} 
+	// TODO debug
+	PORTB |= 8;
     buff_active_flag = 0;
 }
 
 // RTS pin change interrupt
 ISR(PCINT1_vect)
 {
+	// TODO debug
+	PORTB |= 16;
     // dissable pinchange interrupt
     PCICR &= ~(1 << PCIE1);
     sei();
@@ -185,6 +186,8 @@ ISR(PCINT1_vect)
 // UART tx reg empty
 ISR(USART3_UDRE_vect)
 {
+	// TODO debug
+	PORTB |= 128;
     // dissable reg empty interrupt
     UCSR3B &= ~(1 << UDRIE3);
     sei();
